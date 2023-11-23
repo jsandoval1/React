a006f66d67f94198c3aae3461699aca62dd9a765
John Sandoval <jcsandoval978@gmail.com>
2023-11-22 21:51:32 -0500

    Finished on ItsTimeToDuel

--- ItsTimeToDuel.js
+++ ItsTimeToDuel.js
@@ -1,3 +1,4 @@
+// Class of Ninja, only arguement is the name
 class Ninja {
     constructor(name) {
         this.name = name;
@@ -6,6 +7,7 @@ class Ninja {
         this.strength = 3;
     }
 
+    // Methods
     sayName() {
         console.log(`My name is ${this.name}`);
     }
@@ -14,33 +16,33 @@ class Ninja {
         console.log(`My speed is ${this.speed}` + ` and my strength is ${this.strength}. My health is ${this.health}`);
     }
 
-    drinkSake() {
-        this.health += 10;
-        console.log(`My health is now ${this.health} because I drank sake!`);
+    hardAlgorithm() {
+        this.cost -= 2;
+        this.resilience += 3;
+        console.log(`My resilience is now ${this.resilience} because I did a hard algorithm!`);
     }
 
+    unhandledPromiseRejection() {
+        this.cost -= 1;
+        this.resilience -= 2;
+        console.log(`My resilience is now ${this.resilience} because I did an unhandled promise rejection!`);
+    }
+
+    pairProgramming() {
+        this.cost += 3;
+        this.power += 2;
+        console.log(`My power is now ${this.power} and my resilience is now ${this.resilience} because I did pair programming!`);
+    }
+
+    // Attack method that takes in a target as an arguement
     attack(target) {
         const damage = this.strength * 5;
         target.health -= damage;
         console.log(`${this.name} attacked ${target.name} for ${damage} damage!`);
     }
-
-    hardAlgorithm() {
-        this.strength += 3;
-        console.log(`My strength is now ${this.strength} because I completed the hard algorithm!`);
-    }
-
-    unhandledPromiseRejection() {
-        this.health -= 5;
-        console.log(`My health is now ${this.health} because I didn't handle my promise rejection!`);
-    }
-
-    pairProgramming() {
-        this.speed += 10;
-        console.log(`My speed is now ${this.speed} because I did pair programming!`);
-    }
 }
 
+// RedBeltNinja class that extends the Ninja class
 class RedBeltNinja extends Ninja {
     constructor(name) {
         super(name);
@@ -50,6 +52,7 @@ class RedBeltNinja extends Ninja {
     }
 }
 
+// BlackBeltNinja class that extends the Ninja class
 class BlackBeltNinja extends Ninja {
     constructor(name) {
         super(name);
