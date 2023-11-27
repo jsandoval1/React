ba12bffce97cf151b01afcf45a2e89170b2b0bbf
John Sandoval <jcsandoval978@gmail.com>
2023-11-26 23:41:51 -0500

    Cleaned up app.jsx for readablitity

--- ReactBasics/MoreForms/src/App.jsx
+++ ReactBasics/MoreForms/src/App.jsx
@@ -8,12 +8,10 @@ function App() {
       <div className="App">
         {/* For this example, we'll only be using the UserForm component, it has the JSON stringify to display the state */}
         <h1>More Forms</h1>
-        <p>Below is the start of the user form component</p>
         <fieldset>
           <legend>UserForm Component</legend>
           <UserForm />
         </fieldset>
-        <p>Above is the end of the user form component</p>
       </div>
     </>
   )
