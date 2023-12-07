fc791ff4848648af7c1618e800b68c4e8635d6f7
John Sandoval <jcsandoval978@gmail.com>
2023-12-07 01:22:18 -0500

    Add homeworld link to PeopleDisplay component

--- Routing/LukeAPIWalker/src/components/PeopleDisplay.jsx
+++ Routing/LukeAPIWalker/src/components/PeopleDisplay.jsx
@@ -1,41 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, Navigate } from 'react-router-dom';
 import axios from 'axios';
 
 function PeopleDisplay() {
-    // Get the params from the URL
-    const { id } = useParams()
-
-    // Set the state for the person and loading
-    const [people, setPeople] = useState({}) // Empty object
+    const { id } = useParams() 
+    const [people, setPeople] = useState({})
+    const [planet, setPlanet] = useState({}) // we need to store the planet data in state so we can display it
     const [loading, setLoading] = useState(true)
-    
-    // Axios to get the data from the API
+
     useEffect(() => {
+        setLoading(true);
         axios.get(`https://swapi.dev/api/people/${id}`)
             .then(response => {
-                setPeople(response.data)
-                setLoading(false) // Set loading to false when data arrives
+                setPeople(response.data);
+                return axios.get(response.data.homeworld); // returns a promise that we can chain .then() to
             })
-            .catch(err => console.log(err))
-    }, [id])
+            .then(response => {
+                setPlanet(response.data); // set the planet state to the homeworld data
+                setLoading(false); // set loading to false since we got all the data we needed
+            })
+            .catch(err => console.log(err));
+    }, [id]);
 
     return (
         <fieldset>
             <legend>PeopleDisplay.jsx Component</legend>
-            {/* Display loading message if data is not yet arrived */}
             {loading ? (
                 <p>Loading...</p>
             ) : (
                 <div className="peopleDisplay">
-                    {/* Displaying the persons Height, Mass, Hair Color and Skin Color */}
+                    
+                    {/* Display the information of the person for the ID */}
                     <h2>{people.name}</h2>
                     <p>Height: {people.height} cm </p>
                     <p>Mass: {people.mass} kg </p>
                     <p>Hair Color: {people.hair_color}</p>
-                    <p> Skin Color: {people.skin_color} </p>
+                    <p>Skin Color: {people.skin_color}</p>
 
-                    {/* Back to the form */}
+                    {/* If the person has a homeworld, display the name and link to the planet */}
+                    <p>Homeworld: {people.homeworld ? <Link to={`/planets/${planet.id}`}>{planet.name}</Link> : "Unknown"}</p>
+
                     <Link to="/">Back to the form</Link>
                 </div>
             )}
