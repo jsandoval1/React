a2c69563c99397f92cc46a2134373d4e9856024e
John Sandoval <jcsandoval978@gmail.com>
2023-12-13 01:19:19 -0500

    Testing backend server functionality

--- Express/FullStackMern/FullStackPractice/server/server.js
+++ Express/FullStackMern/FullStackPractice/server/server.js
@@ -3,4 +3,6 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
 
+require('./routes/person.routes')(app); // This is new
 app.listen(port, () => console.log(`Listening on port: ${port}`));
+
