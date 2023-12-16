e1fa603382aba72925d3c6fc308d714854f3bad2
John Sandoval <jcsandoval978@gmail.com>
2023-12-15 20:48:35 -0500

    Add link to view all products in the database

--- FullStackMern/ProductManager/client/src/App.jsx
+++ FullStackMern/ProductManager/client/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Link } from 'react-router-dom'
 import Main from './views/Main'
 import ProductDetail from './views/ProductDetail'
 
@@ -10,6 +10,11 @@ function App() {
     <div className="app">
       <h1> Product Manager </h1>
       <h5> Currently looking at App.jsx containing displaying the views. </h5>
+      <p>
+        You can visit
+        <Link to="http://localhost:8000/api/products"> localhost:8000/api/products </Link>
+        to view all the products in the database if you have the server running.
+      </p>
       <h6> "/" is the Main.jsx view which displays the ProductForm.jsx(Component) to create a new products. </h6>
       <h6> "/products/:id" is the ProductDetail.jsx view which displays a single product based on the id passed in the url. </h6>
       <h6> "*" is the 404: Not Found page. This is the default page if the route path does not match. </h6>
