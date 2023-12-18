e5a03d084d7a90a69338113ab868d73c9b567bc4
John Sandoval <jcsandoval978@gmail.com>
2023-12-18 00:31:45 -0500

    Refactor ProductForm component to make clearer on user end

--- FullStackMern/ProductManager/client/src/components/ProductForm.jsx
+++ FullStackMern/ProductManager/client/src/components/ProductForm.jsx
@@ -21,34 +21,38 @@ const ProductForm = (props) => {
     }
 
     return (
-        <form onSubmit={onSubmitHandler}>
-            <p>
-                <label>Name</label><br />
-                <input
-                    type="text"
-                    onChange={(e) => setName(e.target.value)}
-                    value={name}
-                />
-            </p>
-            <p>
-                <label>Price</label><br
-                />
-                <input
-                    type="number"
-                    onChange={(e) => setPrice(e.target.value)}
-                    value={price}
-                />
-            </p>
-            <p>
-                <label>Description</label><br />
-                <input
-                    type="text"
-                    onChange={(e) => setDescription(e.target.value)}
-                    value={description}
-                />
-            </p>
-            <input type="submit" />
-        </form>
+        <fieldset>
+            <legend> ProductForm.jsx Component </legend>
+            <h6> ProductForm.jsx is a reusable component that is used in the Main.jsx and EditProduct.jsx views. </h6>
+            <form onSubmit={onSubmitHandler}>
+                <p>
+                    <label>Name</label><br />
+                    <input
+                        type="text"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                    />
+                </p>
+                <p>
+                    <label>Price</label><br
+                    />
+                    <input
+                        type="number"
+                        onChange={(e) => setPrice(e.target.value)}
+                        value={price}
+                    />
+                </p>
+                <p>
+                    <label>Description</label><br />
+                    <input
+                        type="text"
+                        onChange={(e) => setDescription(e.target.value)}
+                        value={description}
+                    />
+                </p>
+                <input type="submit" />
+            </form>
+        </fieldset>
     )
 }
 
