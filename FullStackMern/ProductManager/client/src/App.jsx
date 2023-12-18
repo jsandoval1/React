e102a3b59120665e1d71d94c834b0b4d743ccdc8
John Sandoval <jcsandoval978@gmail.com>
2023-12-17 21:26:58 -0500

    Added links and routes to different views in App.jsx

--- FullStackMern/ProductManager/client/src/App.jsx
+++ FullStackMern/ProductManager/client/src/App.jsx
@@ -10,25 +10,28 @@ function App() {
   return (
     <div className="app">
       <h1> Product Manager </h1>
-      <h5> Currently looking at App.jsx containing displaying the views. </h5>
-      <p>
-        You can visit
-        <Link to="http://localhost:8000/api/products"> localhost:8000/api/products </Link>
-        to view all the products in the database if you have the server running.
-      </p>
-      <h6> "/" is the Main.jsx view which displays the ProductForm.jsx(Component) to create a new products. </h6>
-      <h6> "/products/:id" is the ProductDetail.jsx view which displays a single product based on the id passed in the url. </h6>
-      <h6> "/products/:id/edit" is the EditProduct.jsx view which displays a form to edit a single product based on the id passed in the url. </h6>
-      <h6> "*" is the 404: Not Found page. This is the default page if the route path does not match. </h6>
+      <h5> Currently looking at App.jsx containing displaying the views. The views will change depending on the url. </h5>
 
+      {/* Added links to the different views */}
+      <h6> Go to <Link to="/"> http://localhost:5173/ </Link> to view the ProductForm.jsx(Component) to create a new product. </h6>
+      <h6> Go to <Link to="/products/657a9497b75580bc6f1d22aa"> http://localhost:5173/products/657a9497b75580bc6f1d22aa </Link> to view a single product. </h6>
+      <h6> Go to <Link to="/products/657a9497b75580bc6f1d22aa/edit"> http://localhost:5173/products/657a9497b75580bc6f1d22aa/edit </Link> to edit a single product. </h6>
+      <h6> Go to <Link to="/random"> http://localhost:5173/random </Link> to view the 404: Not Found page. </h6>
+
+      {/* Added routes to the different views */}
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/products/:id" element={<ProductDetail />} />
         <Route path="/products/:id/edit" element={<EditProduct />} /> {/* Not passing in any props */}
         <Route path="*" element={<h1>404: Not Found</h1>} />
-
       </Routes>
 
+      {/* Added link to view all products in the database as Raw JSON */}
+      <p>
+        You can visit
+        <Link to="http://localhost:8000/api/products"> localhost:8000/api/products </Link>
+        to view all the products in the database as Raw JSON if you have the server running.
+      </p>
     </div>
   )
 }
