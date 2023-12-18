9fa3071334f89e60061db8b84c18b29b360a3ca3
John Sandoval <jcsandoval978@gmail.com>
2023-12-18 00:22:56 -0500

    Refactor EditProduct component and add Helmet for page title, added pseudo

--- FullStackMern/ProductManager/client/src/views/EditProduct.jsx
+++ FullStackMern/ProductManager/client/src/views/EditProduct.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import ProductForm from '../components/ProductForm'
 import { useParams, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Helmet } from 'react-helmet'
 import axios from 'axios'
+import ProductForm from '../components/ProductForm'
+
+// * Notes:
+// We are reusing the ProductForm component => DRY
+// This is done by passing in the onSubmitProp function and the initial values as props
+// where we are using the ProductForm component in the return statement
 
 const EditProduct = (props) => {
     // Params and navigate
@@ -26,21 +32,28 @@ const EditProduct = (props) => {
     // Update the product passed in as an argument
     const updateProduct = product => {
         axios.patch(`http://localhost:8000/api/products/${id}`, product)
-        .then(res => {
-            console.log(res);
-            navigate(`/products/${id}`); // Navigate to the details page
-        })
-        .catch(err => console.error(err));
+            .then(res => {
+                console.log(res);
+                navigate(`/products/${id}`); // Navigate to the details page
+            })
+            .catch(err => console.error(err));
     }
 
     return (
         <div className="editProduct">
+            {/* Helmet to set the title of the page */}
+            < Helmet >
+                <title> {product.name ? 'Edit ' + product.name : 'Loading...'} </title>
+            </Helmet >
+
+            {/* Conditional rendering to display the product information or a loading message */}
             <fieldset>
                 <legend> EditProduct.jsx View </legend>
                 {loaded ? (
                     <ProductForm
-                        onSubmitProp={updateProduct}
-                        initialName={product.name}
+                    //* Pass in the onSubmitProp function and the initial values as props
+                        onSubmitProp={updateProduct} // onSubmitProp is the updateProduct function
+                        initialName={product.name} // initialName, initialPrice, and initialDescription are the product information
                         initialPrice={product.price}
                         initialDescription={product.description}
                     />
@@ -48,6 +61,7 @@ const EditProduct = (props) => {
                     <p> Retrieving product information.. </p>
                 )}
             </fieldset>
+
         </div>
     )
 }
