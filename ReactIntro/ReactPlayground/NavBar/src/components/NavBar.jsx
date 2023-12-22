d380270dc99123f5c634aec14704bcd330aaa8ff
John Sandoval <jcsandoval978@gmail.com>
2023-12-22 12:26:14 -0500

    Trying to add responsive navigation menu

--- ReactIntro/ReactPlayground/NavBar/src/components/NavBar.jsx
+++ ReactIntro/ReactPlayground/NavBar/src/components/NavBar.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/NavBar.css';
 
 function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+        console.log(`Menu is now ${isOpen ? 'closed' : 'open'}`);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false);
+        console.log('Menu is now closed');
+    };
+
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
-        </nav>
+        <div className={`nav-bar ${isOpen ? 'open' : ''}`}>
+            <button onClick={toggleMenu}>
+                {isOpen ? 'Close' : 'Open'} Menu
+            </button>
+            {isOpen && (
+                <nav>
+                    <ul>
+                        <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+                        <li><Link to="/about" onClick={closeMenu}>About</Link></li>
+                        <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
+                    </ul>
+                </nav>
+            )}
+        </div>
     );
 }
 
