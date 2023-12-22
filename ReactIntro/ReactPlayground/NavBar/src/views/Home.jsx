a1058249839905706ffaf73cefb99f3e037c8819
John Sandoval <jcsandoval978@gmail.com>
2023-12-22 12:09:23 -0500

    Add Home view component

---
+++ ReactIntro/ReactPlayground/NavBar/src/views/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+function Home() {
+    return (
+        <fieldset>
+            <legend>Home View, Home.jsx </legend>
+            <h1>Welcome to the Home Page!</h1>
+            <p>
+                React is a JavaScript library for building user interfaces. It allows you to create reusable UI components and efficiently update and render them based on the application state. With React, you can build interactive and dynamic web applications with ease.
+            </p>
+            <p>
+                Whether you are a beginner or an experienced developer, React provides a powerful and flexible framework to develop modern web applications. It follows a component-based architecture, making it easy to manage and organize your code. React also has a vibrant ecosystem with a wide range of libraries and tools to enhance your development experience.
+            </p>
+            <p>
+                Start exploring React and unleash your creativity in building amazing web applications. Happy coding!
+            </p>
+        </fieldset>
+    )
+}
+
+
+export default Home
