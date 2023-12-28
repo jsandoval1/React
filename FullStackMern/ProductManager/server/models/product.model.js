cba04ea5ffed423dbe3c929b1e5c81b15972dd44
John Sandoval <jcsandoval978@gmail.com>
2023-12-28 18:07:38 -0500

    Add validation to product model

--- FullStackMern/ProductManager/server/models/product.model.js
+++ FullStackMern/ProductManager/server/models/product.model.js
@@ -2,13 +2,20 @@
 const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema({
     name: {
-        type: String
+        type: String, 
+        required: [true, "Product name is required"],
+        minlength: [3, "Product name must be at least 3 characters long"]
     },
     price: {
-        type: Number
+        type: Number,
+        required: [true, "Product price is required"],
+        min: [0, "Product price must be greater than 0"],
+        max: [100, "Product price must be less than 100"]
     },
     description: {
-        type: String
+        type: String,
+        required: [true, "Product description is required"],
+        minlength: [3, "Product description must be at least 3 characters long"]
     }
 }, { timestamps: true });
 
