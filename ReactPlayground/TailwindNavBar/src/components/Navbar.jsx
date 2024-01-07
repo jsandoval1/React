7ca607ea4257b297d97ca513c5cd63105a58d470
John Sandoval <jcsandoval978@gmail.com>
2024-01-07 01:52:10 -0500

    Refactor Navbar component to use NavLink and add active styling

--- ReactPlayground/TailwindNavBar/src/components/Navbar.jsx
+++ ReactPlayground/TailwindNavBar/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { GiHamburgerMenu } from 'react-icons/gi'; // Hamburger menu icon from react-icons
-import { FaTimes } from 'react-icons/fa'; // Close icon from react-icons
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaTimes } from 'react-icons/fa';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
 
 function Navbar() {
     // State to manage the open/closed state of the mobile menu
     const [isOpen, setIsOpen] = useState(false);
+    const location = useLocation();
 
     // Function to toggle the mobile menu
     const toggleMenu = () => {
@@ -23,27 +26,26 @@ function Navbar() {
                 {/* Desktop menu */}
                 <h1 className="text-2xl font-bold">Navbar</h1>
                 <ul className="space-x-4 hidden md:flex">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+                    <li><NavLink exact to="/" style={location.pathname === "/" ? { color: "red" } : null} onClick={closeMenu}>Home</NavLink></li>
+                    <li><NavLink to="/about" style={location.pathname === "/about" ? { color: "red" } : null} onClick={closeMenu}>About</NavLink></li>
+                    <li><NavLink to="/contact" style={location.pathname === "/contact" ? { color: "red" } : null} onClick={closeMenu}>Contact</NavLink></li>
                 </ul>
                 {/* Mobile menu icon */}
                 <div className='md:hidden z-10' onClick={toggleMenu}> {/* Icon to be shown on small screens */}
                     {isOpen ? <FaTimes size={32} color='red' onClick={toggleMenu} /> : <GiHamburgerMenu onClick={toggleMenu} />}
                 </div>
                 {/* Mobile menu links */}
-                <ul className={`${
-                    isOpen
-                        ? `text-white opacity-100 transform translate-x-0`
-                        : `opacity-0 transform -translate-y-full`} transition transform
+                <ul className={`${isOpen
+                    ? `text-white opacity-100 transform translate-x-0`
+                    : `opacity-0 transform -translate-y-full`} transition transform
                     absolute top-0 left-0 w-full h-screen bg-zinc-800/60 flex 
                     flex-col items-center justify-center space-y-4 md:hidden text-2xl font-bold`}>
-                    <li><Link to="/" onClick={closeMenu}>Home</Link></li>
-                    <li><Link to="/about" onClick={closeMenu}>About</Link></li>
-                    <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
-            </ul>
+                    <li><NavLink exact to="/" style={location.pathname === "/" ? { color: "red" } : null} onClick={closeMenu}>Home</NavLink></li>
+                    <li><NavLink to="/about" style={location.pathname === "/about" ? { color: "red" } : null} onClick={closeMenu}>About</NavLink></li>
+                    <li><NavLink to="/contact" style={location.pathname === "/contact" ? { color: "red" } : null} onClick={closeMenu}>Contact</NavLink></li>
+                </ul>
+            </div>
         </div>
-        </div >
     )
 }
 
