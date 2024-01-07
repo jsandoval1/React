80891c32873d4069b52f851d578dc6c2801bb065
John Sandoval <jcsandoval978@gmail.com>
2024-01-06 23:28:29 -0500

    First Commit

---
+++ ReactPlayground/TailwindNavBar/src/App.jsx
@@ -0,0 +1,18 @@
+import { FaReact } from 'react-icons/fa';
+import './App.css';
+
+// Notes:
+// Installed react-icons, Tailwind CSS, and react-router-dom
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+        <FaReact />
+      </h1>
+    </>
+  )
+}
+
+export default App;
