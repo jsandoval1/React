32c0bda6622d5c0c2fd6117aba56746cbe0cda65
John Sandoval <jcsandoval978@gmail.com>
2024-01-07 00:13:02 -0500

    Update Navbar and Home components

--- ReactPlayground/TailwindNavBar/src/components/Navbar.jsx
+++ ReactPlayground/TailwindNavBar/src/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom';
+
 function Navbar() {
     return (
         <div className="bg-gray-400 p-4">
-            Navbar
+            <h1 className="text-2xl text-center font-bold">Navbar</h1>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
         </div>
     )
 }
--- ReactPlayground/TailwindNavBar/src/views/Home.jsx
+++ ReactPlayground/TailwindNavBar/src/views/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 function Home() {
     return (
-        <div className="bg-gray-200 p-4">
-            <h1 className="text-2xl font-bold mb-2">Welcome to our Fake Home Page!</h1>
-            <p className="text-gray-600">This is just a placeholder for demonstration purposes.</p>
+        <div className="flex flex-col items-center justify-center h-auto p-4 md:p-12 lg:p-16 bg-blue-200">
+            <div className="text-center w-full mx-0 md:mx-12 lg:mx-16 bg-white">
+                <h1 className="text-2xl font-bold mb-2 text-blue-800">Welcome to our Fake Home Page!</h1>
+                <p className="text-gray-800">This is just a placeholder for demonstration purposes.</p>
+            </div>
         </div>
     );
 }
 
-export default Home
+export default Home;
