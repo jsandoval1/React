efe05fa5d9a3d85652df2ef81cfc3bdc993e6dfd
John Sandoval <jcsandoval978@gmail.com>
2024-01-14 22:50:50 -0500

    Created User Model

---
+++ ReactPlayground/ChatApp/server/models/user.model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        minlength: [3, "Username must be at least 3 characters long"],
+        maxlength: [20, "Username must be less than 20 characters long"],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        minlength: [3, "Email must be at least 3 characters long"],
+        maxlength: [50, "Email must be less than 50 characters long"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [8, "Password must be at least 8 characters long"],
+        maxlength: [20, "Password must be less than 20 characters long"]
+    },
+    profilePictrure: {
+        type: String,
+        default: ""
+    },
+    coverPicture: {
+        type: String,
+        default: ""
+    },
+    followers: {
+        type: Array,
+        default: []
+    },
+    following: {
+        type: Array,
+        default: []
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
