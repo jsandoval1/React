67b3eeb838c79a3ae881fbcd527e197591712ca7
John Sandoval <jcsandoval978@gmail.com>
2024-01-18 18:19:21 -0500

    Delete message-related files and routes

--- ReactPlayground/ChatApp/server/routes/message.routes.js
+++
@@ -1,5 +0,0 @@
-const messageController = require('../controllers/message.controller');
-module.exports = function(app) {
-    app.get('/api/messages', messageController.getMessages);
-    app.post('/api/messages', messageController.createMessage);
-}
