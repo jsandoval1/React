d277348b1ce0cc2b67c0241c2df4fb447366df86
John Sandoval <jcsandoval978@gmail.com>
2024-01-18 23:20:20 -0500

    Add demo id to userActions.routes.js

--- ReactPlayground/ChatApp/server/routes/userActions.routes.js
+++ ReactPlayground/ChatApp/server/routes/userActions.routes.js
@@ -1,5 +1,7 @@
 const userActions = require("../controllers/userActions.controller");
 
+// DEMO id: 65a9bd025bd40c6152e2115f
+
 module.exports = function(app) {
     app.put("/api/users/:id", userActions.updateUser);
 }
