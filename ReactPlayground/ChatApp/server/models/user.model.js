3ab6594840bff4d45df59774d24fea7f4a1e7be4
John Sandoval <jcsandoval978@gmail.com>
2024-01-18 23:19:33 -0500

    Update email and password validation in user.model.js

--- ReactPlayground/ChatApp/server/models/user.model.js
+++ ReactPlayground/ChatApp/server/models/user.model.js
@@ -14,12 +14,15 @@ const UserSchema = new mongoose.Schema({
         minlength: [3, "Email must be at least 3 characters long"],
         maxlength: [50, "Email must be less than 50 characters long"],
         unique: true,
-        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+        // todo: Regex commented out for testing purposes
+        // match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
     },
     password: {
         type: String,
         required: [true, "Password is required"],
-        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, "Password must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 special character"]
+        minlength: [8, "Password must be at least 8 characters long"],
+        // todo: Regex commented out for testing purposes
+        // match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, "Password must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 special character"]
     },
     profilePictrure: {
         type: String,
