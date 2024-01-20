99db544b2e70ce4426bb4ef7fa0c09a621bc177b
John Sandoval <jcsandoval978@gmail.com>
2024-01-20 01:12:34 -0500

    started working on front end to build for testing sockets

--- ReactPlayground/ChatApp/client/src/App.jsx
+++ ReactPlayground/ChatApp/client/src/App.jsx
@@ -1,28 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import axios from './config/axiosConfig';
 import './App.css';
 
-function App() {
-  const [messages, setMessages] = useState([]);
+import HomePage from './views/HomePage';
 
-  useEffect(() => {
-    axios.get('api/messages')
-      .then(response => {
-        setMessages(response.data);
-      })
-      .catch(error => {
-        console.error('There was an error!', error);
-      });
-  }, []);
+function App() {
 
   return (
     <div>
-      {messages.map((message, index) => (
-        <div key={index}>
-          <p>{message.message}</p> {/* Access the message property of each message object */}
-          <p>{message.date}</p> {/* Access the date property of each message object */}
-        </div>
-      ))}
+      <HomePage />
     </div>
   );
 }
---
+++ ReactPlayground/ChatApp/client/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { FaBell, FaRegComment, FaSearch, FaUser } from 'react-icons/fa'
+import './Navbar.css'
+
+
+function Navbar() {
+    return (
+        <div className="navbarContainer">
+            <div className="navbarLeft">
+                <span> Sandoval </span>
+            </div>
+
+            <div className="navbarCenter">
+                <div className="searchBar">
+                    <FaSearch className="searchIcon" />
+                    <input type="text" placeholder="Search for something" />
+                </div>
+            </div>
+
+            <div className="navbarRight">
+                <div className="navbarLinks">
+                    <span> Home </span>
+                    <span> Timeline </span>
+                </div>
+                <div className="navbarIcons">
+                    <div className="navbarIconItem">
+                        <FaUser />
+                        <span className="navbarIconBadge"> 1 </span>
+                    </div>
+                    < div className="navbarIconItem">
+                        <FaRegComment />
+                        <span className="navbarIconBadge"> 2 </span>
+                    </div>
+                    <div className="navbarIconItem">
+                        <FaBell />
+                        <span className="navbarIconBadge"> 1 </span>
+                    </div>
+                </div>
+                {/* < img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="navbarImg" /> */}
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
