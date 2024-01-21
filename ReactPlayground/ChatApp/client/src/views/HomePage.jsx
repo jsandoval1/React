fd7f3941b6e308a9ee1adb1a67b62b24aad40acb
John Sandoval <jcsandoval978@gmail.com>
2024-01-21 12:58:04 -0500

    Add HomePage.css and update HomePage.jsx

--- ReactPlayground/ChatApp/client/src/views/HomePage.jsx
+++ ReactPlayground/ChatApp/client/src/views/HomePage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import Sidebar from '../components/Sidebar/Sidebar'
+import Feed from '../components/Feed/Feed'
+import Rightbar from '../components/Rightbar/Rightbar'
+
+import './HomePage.css'
 
 function HomePage() {
     return (
+        <>
             <Navbar />
+            <div className="homeContainer">
+                <Sidebar />
+                <Feed />
+                <Rightbar />
+            </div>
+        </>
     )
 }
 
