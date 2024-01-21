281830072b681082618e316580dacfe495a17742
John Sandoval <jcsandoval978@gmail.com>
2024-01-21 12:57:49 -0500

    Created Rightbar component

---
+++ ReactPlayground/ChatApp/client/src/components/Rightbar/Rightbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Rightbar.css'
+
+function Rightbar() {
+    return (
+        <div className="rightbarContainer">
+            Rightbar
+        </div>
+    )
+}
+
+export default Rightbar
