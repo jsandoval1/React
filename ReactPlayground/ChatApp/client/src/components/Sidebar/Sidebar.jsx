443194407407aaad43a5cd43720691349443d0e2
John Sandoval <jcsandoval978@gmail.com>
2024-01-22 00:03:26 -0500

    Made a subcomponent

--- ReactPlayground/ChatApp/client/src/components/Sidebar/Sidebar.jsx
+++ ReactPlayground/ChatApp/client/src/components/Sidebar/Sidebar.jsx
@@ -1,49 +1,97 @@
 import React from 'react'
 import './Sidebar.css'
+import CloseFriend from '../closeFriend/closeFriend'
 
 import { FaRss, FaUserFriends, FaBookmark, FaCalendarWeek, FaUserCog, FaUserShield, FaUserPlus, FaUserMinus, FaUserTimes, FaUserCheck, FaUserEdit, FaUserSlash, FaUser, FaUsers, FaUserTag, FaUserTie, FaUserNurse, FaUserMd, FaUserInjured, FaUserAstronaut, FaUserSecret, FaUserGraduate, FaUserNinja, FaUserLock, FaVideo } from 'react-icons/fa'
+
+// Dummy data for close friends
+const user = [
+    {
+        id: 1,
+        profilePicture: "https://via.placeholder.com/150",
+        username: "John Sandoval"
+    },
+    {
+        id: 2,
+        profilePicture: "https://via.placeholder.com/150",
+        username: "Trevor Haddad"
+    },
+    {
+        id: 3,
+        profilePicture: "https://via.placeholder.com/150",
+        username: "Matt Rafferty"
+    },
+    {
+        id: 4,
+        profilePicture: "https://via.placeholder.com/150",
+        username: "Chris Gomes"
+    },
+    {
+        id: 5,
+        profilePicture: "https://via.placeholder.com/150",
+        username: "Jesse Rios"
+    },
+    {
+        id: 6,
+        profilePicture: "https://via.placeholder.com/150",
+        username: "Dylan Cody"
+    },
+    {
+        id: 7,
+        profilePicture: "https://via.placeholder.com/150",
+        username: "Tanya Jean"
+    },
+    {
+        id: 8,
+        profilePicture: "https://via.placeholder.com/150",
+        username: "David Khom"
+    },
+    {
+        id: 9,
+        profilePicture: "https://via.placeholder.com/150",
+        username: "Chris Pantanos"
+    },
+    {
+        id: 10,
+        profilePicture: "https://via.placeholder.com/150",
+        username: "Lasco Jones"
+    }
+]
+
 
 function Sidebar() {
     return (
         <div className="sidebarContainer">
             <div className="sidebarWrapper">
                 <ul className="sidebarList">
-                    {/* Profile */}
                     <li className="sidebarListItem">
                         <FaRss className="sidebarIcon" />
                         <span className="sidebarListItemText">Feed</span>
                     </li>
-                    {/* Messages */}
                     <li className="sidebarListItem">
                         <FaUserFriends className="sidebarIcon" />
                         <span className="sidebarListItemText">Chats</span>
                     </li>
-                    {/* Videos */}
                     <li className="sidebarListItem">
                         <FaVideo className="sidebarIcon" />
                         <span className="sidebarListItemText">Videos</span>
                     </li>
-                    {/* Groups */}
                     <li className="sidebarListItem">
                         <FaUsers className="sidebarIcon" />
                         <span className="sidebarListItemText">Groups</span>
                     </li>
-                    {/* Bookmarks */}
                     <li className="sidebarListItem">
                         <FaBookmark className="sidebarIcon" />
                         <span className="sidebarListItemText">Bookmarks</span>
                     </li>
-                    {/* Questions */}
                     <li className="sidebarListItem">
                         <FaCalendarWeek className="sidebarIcon" />
                         <span className="sidebarListItemText">Questions</span>
                     </li>
-                    {/* Jobs */}
                     <li className="sidebarListItem">
                         <FaUserCog className="sidebarIcon" />
                         <span className="sidebarListItemText">Jobs</span>
                     </li>
-                    {/* Courses */}
                     <li className="sidebarListItem">
                         <FaUserShield className="sidebarIcon" />
                         <span className="sidebarListItemText">Courses</span>
@@ -54,81 +102,9 @@ function Sidebar() {
                 <hr className="sidebarHr" />
 
                 <ul className="sidebarFriendList">
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">John Doe</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Mark Brown</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Eric King</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">John Doe</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Mark Brown</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Eric King</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">John Doe</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Mark Brown</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Eric King</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">John Doe</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Mark Brown</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Eric King</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">John Doe</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Mark Brown</span>
-                    </li>
-                    {/* Add Friend */}
-                    <li className="sidebarFriend">
-                        <img src="https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" className="sidebarFriendImg" />
-                        <span className="sidebarFriendName">Eric King</span>
-                    </li>
+                    {user.map((u) => (
+                        <CloseFriend key={u.id} user={u} />
+                    ))}
                 </ul>
 
             </div>
