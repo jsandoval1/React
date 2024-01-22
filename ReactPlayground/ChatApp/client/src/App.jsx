87e79bd8e5e2537a56fddff30267eae58bdeb5c1
John Sandoval <jcsandoval978@gmail.com>
2024-01-22 12:20:12 -0500

    Add Profile component to App.jsx

--- ReactPlayground/ChatApp/client/src/App.jsx
+++ ReactPlayground/ChatApp/client/src/App.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import HomePage from './views/HomePage';
+import Profile from './views/Profile';
 
 function App() {
 
   return (
     <div>
-      <HomePage />
+      {/* <HomePage /> */}
+      <Profile />
     </div>
   );
 }
