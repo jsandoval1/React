155ea18e600a6c753087b6bbfb9b7d1e538e8dba
John Sandoval <jcsandoval978@gmail.com>
2024-01-22 15:38:09 -0500

    Add headers and update page content in App.jsx to make sure all views work

--- ReactPlayground/ChatApp/client/src/App.jsx
+++ ReactPlayground/ChatApp/client/src/App.jsx
@@ -8,11 +8,26 @@ import Register from './views/Register/Register';
 
 function App() {
 
+  // style function for the headers
+  const getHeaderStyle = () => {
+    return { 
+      backgroundColor: 'lightgray', 
+      padding: '10px', 
+      fontSize: '2em', 
+      color: 'darkblue', 
+      border: '2px solid darkblue' 
+    };
+  };
+
   return (
     <div>
-      {/* <HomePage /> */}
-      {/* <Profile /> */}
-      {/* <Login /> */}
+      <h1 style={getHeaderStyle()}>This should be the homepage</h1>
+      <HomePage />
+      <h1 style={getHeaderStyle()}>This should be the profile page</h1>
+      <Profile />
+      <h1 style={getHeaderStyle()}>This should be the login page</h1>
+      <Login />
+      <h1 style={getHeaderStyle()}>This should be the register page</h1>
       <Register />
     </div>
   );
