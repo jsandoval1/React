20a551d154d49db1320827057fedf9ebc9f774d8
John Sandoval <jcsandoval978@gmail.com>
2024-02-05 18:18:01 -0500

    Add user profile functionality and fetch user data

--- ReactPlayground/ChatApp/client/src/views/Profile/Profile.jsx
+++ ReactPlayground/ChatApp/client/src/views/Profile/Profile.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 import './Profile.css'
+import { useParams } from 'react-router-dom'
+
+import noAvatar from '../../public/images/person/noAvatar.png'
+import noCover from '../../public/images/person/noCover.png'
+import api from '../../config/axiosConfig'
 
 import Navbar from '../../components/Navbar/Navbar'
 import Sidebar from '../../components/Sidebar/Sidebar'
@@ -8,6 +14,17 @@ import Rightbar from '../../components/Rightbar/Rightbar'
 
 
 function Profile() {
+    const [user, setUser] = useState({});
+    const username = useParams().username;
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const res = await api.get(`/users?username=${username}`);
+            setUser(res.data);
+        };
+        fetchUser();
+    }, []);
+
     return (
         <>
             <Navbar />
@@ -16,17 +33,21 @@ function Profile() {
                 <div className="profileRight">
                     <div className="profileRightTop">
                         <div className="profileCover">
-                            <img className="profileCoverImg" src="https://source.unsplash.com/random?mountain" alt="Mountain" />
-                            <img className="profileUserImg" src="https://source.unsplash.com/random?person" alt="User" />
+                            <img className="profileCoverImg"
+                                src= {user.coverPicture || noCover}
+                                alt="Mountain" />
+                            <img className="profileUserImg"
+                                src={user.profilePicture || noAvatar}
+                                alt="User" />
                         </div>
                         <div className="profileInfo">
-                            <h4 className="profileInfoName">John Doe</h4>
-                            <span className="profileInfoDesc">Hello my friends!</span>
+                            <h4 className="profileInfoName">{user.username}</h4>
+                            <span className="profileInfoDesc">{user.desc}</span>
                         </div>
                     </div>
                     <div className="profileRightBottom">
-                        <Feed />
-                        <Rightbar profile />
+                        <Feed username={username} />
+                        <Rightbar user={user} />
                     </div>
                 </div>
             </div>
