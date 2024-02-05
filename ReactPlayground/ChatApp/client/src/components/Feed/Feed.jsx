72e3655983eb3daa28a5961f4ce37fcd19462147
John Sandoval <jcsandoval978@gmail.com>
2024-02-05 15:04:00 -0500

    Refactor Feed component to fetch timeline posts from the server

--- ReactPlayground/ChatApp/client/src/components/Feed/Feed.jsx
+++ ReactPlayground/ChatApp/client/src/components/Feed/Feed.jsx
@@ -1,63 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Feed.css'
 import Share from '../Share/Share'
 import Post from '../Post/Post'
-
-// Dummy data for posts
-const post = [
-    {
-        id: 1,
-        name: "John Sandoval",
-        profilePicture: "https://via.placeholder.com/150",
-        desc: "Love this place!",
-        postPicture: "https://via.placeholder.com/500",
-        date: "5 mins ago",
-        userId: 1,
-        like: 32,
-        comment: 9
-    },
-    {
-        id: 2,
-        name: "Trevor Haddad",
-        profilePicture: "https://via.placeholder.com/150",
-        desc: "Love this place!",
-        postPicture: "https://via.placeholder.com/500",
-        date: "15 mins ago",
-        userId: 2,
-        like: 2,
-        comment: 1
-    },
-    {
-        id: 3,
-        name: "Matt Rafferty",
-        profilePicture: "https://via.placeholder.com/150",
-        desc: "Love this place!",
-        postPicture: "https://via.placeholder.com/500",
-        date: "25 mins ago",
-        userId: 3,
-        like: 12,
-        comment: 2
-    },
-    {
-        id: 4,
-        name: "Chris Gomes",
-        profilePicture: "https://via.placeholder.com/150",
-        desc: "Love this place!",
-        postPicture: "https://via.placeholder.com/500",
-        date: "35 mins ago",
-        userId: 4,
-        like: 52,
-        comment: 5
-    }
-]
+import api from '../../config/axiosConfig'
 
 function Feed() {
+    const [post, setPost] = useState([]);
+
+    // Fetch timeline posts from the server
+    useEffect(() => {
+        const fetchPost = async () => {
+            const res = await api.get("/posts/timeline/65a9fad689e655b361835880");
+            console.log(res.data);
+            setPost(res.data);
+        }
+        fetchPost();
+    }, []);
+
     return (
         <div className="feedContainer">
             <div className="feedWrapper">
                 <Share />
-                {post.map((p) => (
-                    <Post key={p.id} post={p} />
+                {post.map((p, index) => (
+                    <Post key={index} post={p} />
                 ))}
             </div>
         </div>
