256f25e0cc26555a11a60592b30b6e7e93a11ec8
John Sandoval <jcsandoval978@gmail.com>
2024-02-05 18:18:09 -0500

    Refactor post controller responses

--- ReactPlayground/ChatApp/server/controllers/post.controller.js
+++ ReactPlayground/ChatApp/server/controllers/post.controller.js
@@ -8,7 +8,8 @@ module.exports.createPost = async (req, res) => {
     const newPost = new Post(req.body);
     try {
         const savedPost = await newPost.save();
-        res.status(200).json({ message: "Post created successfully", post: savedPost });
+        // res.status(200).json({ message: "Post created successfully", post: savedPost });
+        res.status(200).json(savedPost);
     } catch (err) {
         res.status(500).json(err);
     }
@@ -33,7 +34,8 @@ module.exports.updatePost = async (req, res) => {
 module.exports.getPost = async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        res.status(200).json({ message: "Post retrieved successfully", post: post });
+        // res.status(200).json({ message: "Post retrieved successfully", post: post });
+        res.status(200).json(post);
     } catch (err) {
         res.status(500).json(err);
     }
@@ -61,7 +63,7 @@ module.exports.getUserPosts = async (req, res) => {
     try {
         const user = await User.findOne({ username: req.params.username });
         const posts = await Post.find({ userId: user._id });
-        res.status(200).json({ message: "Posts retrieved successfully", posts: posts });
+        res.status(200).json(posts);
     } catch (err) {
         res.status(500).json(err);
     }
