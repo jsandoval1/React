be562fa582cf44b5b2363aa20d3403e9a80bcb4d
John Sandoval <jcsandoval978@gmail.com>
2024-02-05 15:04:07 -0500

    Refactor Post component to fetch user data and update UI

--- ReactPlayground/ChatApp/client/src/components/Post/Post.jsx
+++ ReactPlayground/ChatApp/client/src/components/Post/Post.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../config/axiosConfig'
+import {format} from 'timeago.js'
+
+import { AiFillLike, AiFillHeart } from 'react-icons/ai';
+import { IoMdMore } from 'react-icons/io';
+
+import noAvatar from '../../public/images/person/noAvatar.png'
 import './Post.css'
 
-import { IoMdMore } from 'react-icons/io';
-import { AiFillLike, AiFillHeart } from 'react-icons/ai';
-
 function Post({ post }) {
-    const [like, setLike] = useState(post.like)
-    const [isLiked, setIsLiked] = useState(false)
+    const [like, setLike] = useState(post.likes.length);
+    const [isLiked, setIsLiked] = useState(false);
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const res = await api.get(`/users/${post.userId}`);
+            setUser(res.data);
+        }
+        fetchUser();
+    }, [post.userId]);
 
     const likeHandler = () => {
         setLike(isLiked ? like - 1 : like + 1)
@@ -19,9 +32,10 @@ function Post({ post }) {
 
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img src={post.profilePicture} alt="" className="postProfileImg" />
-                        <span className="postUsername"> {post.name} </span>
-                        <span className="postDate"> {post.date} </span>
+                        <img src={user.profilePicture || noAvatar}
+                        alt="" className="postProfileImg" />
+                        <span className="postUsername"> {user.username} </span>
+                        <span className="postDate"> {format(post.createdAt)} </span>
                     </div>
                     <div className="postTopRight">
                         <IoMdMore />
@@ -29,14 +43,14 @@ function Post({ post }) {
                 </div>
 
                 <div className="postCenter">
-                    <span className="postText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</span>
-                    <img src={post.postPicture} alt="" className="postImg" />
+                    <span className="postText"> {post.desc} </span>
+                    <img src={post.img} alt="" className="postImg" />
                 </div>
 
                 <div className="postBottom">
                     <div className="postBottomLeft">
                         <AiFillLike className="likeIcon" onClick={likeHandler} color='blue' />
-                        <AiFillHeart className="likeIcon" onClick={likeHandler} color='pink' />
+                        <AiFillHeart className="likeIcon" onClick={likeHandler} color='red' />
                         <span className="postLikeCounter"> {like} people like this </span>
                     </div>
                     <div className="postBottomRight">
