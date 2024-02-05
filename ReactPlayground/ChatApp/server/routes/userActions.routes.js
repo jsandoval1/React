28c7c4f598739aa316c95208dc50ba4f5c408230
John Sandoval <jcsandoval978@gmail.com>
2024-02-05 18:18:21 -0500

    Update userActions.routes.js

--- ReactPlayground/ChatApp/server/routes/userActions.routes.js
+++ ReactPlayground/ChatApp/server/routes/userActions.routes.js
@@ -5,7 +5,10 @@ const userActions = require("../controllers/userActions.controller");
 module.exports = function(app) {
     app.put("/api/users/:id", userActions.updateUser);
     app.delete("/api/users/:id", userActions.deleteUser);
-    app.get("/api/users/:id", userActions.getUser);
+    // ! Not work because using query string
+    // app.get("/api/users/:id", userActions.getUser); 
+    // app.get("/api/:id", userActions.getUser);
+    app.get("/api/users", userActions.getUser);
     app.put("/api/users/:id/follow", userActions.followUser);
     app.put("/api/users/:id/unfollow", userActions.unfollowUser);
 }
