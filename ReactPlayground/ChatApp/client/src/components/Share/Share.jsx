2fecdf3e5b60f7d18342fb4eb0cc2a050c22dad7
John Sandoval <jcsandoval978@gmail.com>
2024-02-27 22:33:28 -0500

    Adding share functionality to Share component

--- ReactPlayground/ChatApp/client/src/components/Share/Share.jsx
+++ ReactPlayground/ChatApp/client/src/components/Share/Share.jsx
@@ -1,27 +1,67 @@
 import React from 'react'
 import './Share.css'
+import { useContext, useRef, useState } from 'react'
+import { AuthContext } from '../../context/AuthContext'
+import noAvatar from '../../public/images/person/noAvatar.png'
 
 import { FaCamera, FaMapMarkerAlt, FaSmile, FaTag } from 'react-icons/fa'
+import axios from 'axios'
 
 function Share() {
+    const { user } = useContext(AuthContext);
+
+    // const [file, setFile] = useState(null);
+
+    const desc = useRef();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const newPost = {
+            userId: user._id,
+            desc: desc.current.value
+        }
+        console.log(newPost);
+
+        // Send the post to the server
+        try {
+            axios.post("/posts", newPost);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <div className="shareContainer">
             <div className="shareWrapper">
 
                 <div className="shareTop">
-                    <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" className="shareProfileImg" />
-                    <input placeholder="What's on your mind?" className="shareInput" />
+                    <img
+                        className="shareProfileImg"
+                        src={user.profilePicture || noAvatar}
+                        alt=""
+                    />
+                    <input
+                        placeholder={"What's in your mind " + user.username + "?"}
+                        className="shareInput"
+                        ref={desc}
+                    />
                 </div>
 
                 <hr className="shareHr" />
 
-                <div className="shareBottom">
+                <form className="shareBottom" onSubmit={handleSubmit}>
 
                     <div className="shareOptions">
-                        <div className="shareOption">
+                        <label htmlFor='file' className="shareOption">
                             <FaCamera className="shareIcon" color='tomato' />
                             <span className="shareOptionText">Media</span>
-                        </div>
+                            <input type="file"
+                                id="file"
+                                accept='.png, .jpeg, .jpg'
+                                style={{ display: "none" }}
+                                onChange={(e) => setFile(e.target.files[0])}
+                            />
+                        </label>
                         <div className="shareOption">
                             <FaTag className="shareIcon" color='blue' />
                             <span className="shareOptionText">Tagged</span>
@@ -36,7 +76,7 @@ function Share() {
                         </div>
                     </div>
                     <button className="shareButton">Share</button>
-                </div>
+                </form>
 
             </div>
         </div>
