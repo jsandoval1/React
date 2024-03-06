489ab65c6a573399596bcade0cd1ac360f2c0868
John Sandoval <jcsandoval978@gmail.com>
2024-03-06 14:58:19 -0500

    Add getUserFriends API endpoint, deleted clutter notes

--- ReactPlayground/ChatApp/server/routes/userActions.routes.js
+++ ReactPlayground/ChatApp/server/routes/userActions.routes.js
@@ -5,10 +5,8 @@ const userActions = require("../controllers/userActions.controller");
 module.exports = function(app) {
     app.put("/api/users/:id", userActions.updateUser);
     app.delete("/api/users/:id", userActions.deleteUser);
-    // ! Not work because using query string
-    // app.get("/api/users/:id", userActions.getUser); 
-    // app.get("/api/:id", userActions.getUser);
     app.get("/api/users", userActions.getUser);
     app.put("/api/users/:id/follow", userActions.followUser);
     app.put("/api/users/:id/unfollow", userActions.unfollowUser);
+    app.get("/api/users/friends/:userId", userActions.getUserFriends);
 }
