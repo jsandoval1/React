4fa6750f870f97622c9bbf9a023227c60a27b0dc
John Sandoval <jcsandoval978@gmail.com>
2024-07-10 22:29:44 -0400

    Remove unnecessary code in userActions.routes.js

--- ReactPlayground/ChatApp/server/routes/userActions.routes.js
+++ ReactPlayground/ChatApp/server/routes/userActions.routes.js
@@ -1,7 +1,5 @@
 const userActions = require("../controllers/userActions.controller");
 
-// DEMO id: 65a9bd025bd40c6152e2115f
-
 module.exports = function(app) {
     app.put("/api/users/:id", userActions.updateUser);
     app.delete("/api/users/:id", userActions.deleteUser);
