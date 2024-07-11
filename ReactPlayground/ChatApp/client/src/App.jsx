12e07bc88f429b6d450793c78da2dd5368c30ab7
John Sandoval <jcsandoval978@gmail.com>
2024-07-10 22:29:17 -0400

    Added fake credentials and also some other credentials

--- ReactPlayground/ChatApp/client/src/App.jsx
+++ ReactPlayground/ChatApp/client/src/App.jsx
@@ -14,13 +14,21 @@ import { AuthContext } from './context/AuthContext';
 // email: "johndoe1@example.com"
 // password: "password"
 
+// username: "janedoe2"
+// email: "janedoe2@example.com"
+// password: "password"
+
+// uswername: "johndoe3"
+// email: "johndoe3@example.com"
+// password: "password"
+
 function App() {
   const {user} = useContext(AuthContext);
 
   return (
     <div>
         <Routes>
-          <Route path="/" element={user ? <HomePage /> : <Register />} />
+          <Route path="/" element={user ? <HomePage /> : <Login />} />
           <Route path="/profile/:username" element={<Profile />} />
           <Route path="/login" element={ user ? <Navigate to="/" /> : <Login />} />
           <Route path="/register" element={ user ? <Navigate to="/" /> : <Register />} />
