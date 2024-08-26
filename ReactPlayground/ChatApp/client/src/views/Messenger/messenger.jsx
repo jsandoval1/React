51b661068b8d07e200fbd4b97c265ff4dcdc6da7
John Sandoval <jcsandoval978@gmail.com>
2024-08-26 14:29:48 -0400

    Add conversation context and connect API/ server  in Messenger view

--- ReactPlayground/ChatApp/client/src/views/Messenger/messenger.jsx
+++ ReactPlayground/ChatApp/client/src/views/Messenger/messenger.jsx
@@ -1,62 +1,81 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import './messenger.css'
 
 import Navbar from '../../components/Navbar/Navbar'
-import Conversation from '../../components/conversation/conversation'
+import Conversation from '../../components/conversation/Conversation'
 import Message from '../../components/message/Message'
 import ChatOnline from '../../components/chatOnline/ChatOnline'
 
-function messenger() {
+import { AuthContext } from '../../context/AuthContext'
+import { useState } from 'react'
+import api from '../../config/axiosConfig'
 
-    return (
-        <>
-            <Navbar />
-            <div className="messenger">
-                <div className="chatMenu">
-                    <div className="chatMenuWrapper">
-                        <input placeholder="Search for friends" className="chatMenuInput" />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                        <Conversation />
-                    </div>
-                </div>
-                <div className="chatBox">
-                    <div className="chatBoxWrapper">
-                        <div className="chatBoxTop">
-                            <Message />
-                            <Message own />
-                            <Message />
-                            <Message />
-                            <Message own />
-                            <Message />
-                            <Message />
-                            <Message own />
-                            <Message />
-                            <Message />
-                            <Message own />
-                            <Message />
-                        </div>
-                        <div className="chatBoxBottom">
-                            <textarea
-                                className="chatMessageInput"
-                                placeholder="Write something...">
-                            </textarea>
-                            <button className="chatSubmitButton">Send</button>
+
+function messenger() {
+    const [conversations, setConversations] = useState([]);
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        const getConversations = async () => {
+            try {
+                const res = await api.get("/conversations/" + user._id)
+                setConversations(res.data)
+                console.log(res.data)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        getConversations()
+    }, [user._id])
+
+        return (
+            <>
+                <Navbar />
+                <div className="messenger">
+                    <div className="chatMenu">
+                        <div className="chatMenuWrapper">
+                            <input placeholder="Search for friends" className="chatMenuInput" />
+                            {conversations.map((c) => (
+                                <Conversation conversation={c} currentUser={user} />
+                            ))}
                         </div>
                     </div>
-                </div>
-                <div className="chatOnline">
-                    <div className="chatOnlineWrapper">
-                        <ChatOnline />
-                        <ChatOnline />
-                        <ChatOnline />
-                        <ChatOnline />
+                    <div className="chatBox">
+                        <div className="chatBoxWrapper">
+                            <div className="chatBoxTop">
+                                <Message />
+                                <Message own />
+                                <Message />
+                                <Message />
+                                <Message own />
+                                <Message />
+                                <Message />
+                                <Message own />
+                                <Message />
+                                <Message />
+                                <Message own />
+                                <Message />
+                            </div>
+                            <div className="chatBoxBottom">
+                                <textarea
+                                    className="chatMessageInput"
+                                    placeholder="Write something...">
+                                </textarea>
+                                <button className="chatSubmitButton">Send</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="chatOnline">
+                        <div className="chatOnlineWrapper">
+                            <ChatOnline />
+                            <ChatOnline />
+                            <ChatOnline />
+                            <ChatOnline />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
-}
+            </>
+        )
+    }
 
 export default messenger
