0d40a868b0abc7cf7c6e3b54302a1bfa8676403f
John Sandoval <jcsandoval978@gmail.com>
2024-08-27 10:17:13 -0400

    Remove success message, was sending object over array

--- ReactPlayground/ChatApp/server/controllers/message.controller.js
+++ ReactPlayground/ChatApp/server/controllers/message.controller.js
@@ -7,7 +7,7 @@ module.exports.createMessage = async (req, res) => {
     const newMessage = new Message(req.body);
     try {
         const savedMessage = await newMessage.save();
-        res.status(200).json({ message: "Message created successfully", message: savedMessage });
+        res.status(200).json(savedMessage);
     } catch (err) {
         res.status(500).json(err);
     }
@@ -17,7 +17,7 @@ module.exports.createMessage = async (req, res) => {
 module.exports.getMessages = async (req, res) => {
     try {
         const messages = await Message.find({ conversationId: req.params.conversationId });
-        res.status(200).json({ message: "Messages retrieved successfully", messages: messages });
+        res.status(200).json(messages);
     } catch (err) {
         res.status(500).json(err);
     }
