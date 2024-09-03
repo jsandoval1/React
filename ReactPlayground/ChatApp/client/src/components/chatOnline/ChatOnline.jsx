d59680150fa2989208077fb9c588313e40da209c
John Sandoval <jcsandoval978@gmail.com>
2024-09-03 13:59:33 -0400

    Working on making component to display online friends

--- ReactPlayground/ChatApp/client/src/components/chatOnline/ChatOnline.jsx
+++ ReactPlayground/ChatApp/client/src/components/chatOnline/ChatOnline.jsx
@@ -1,21 +1,50 @@
 import React from 'react'
 import './chatOnline.css'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import api from '../../config/axiosConfig';
 
-function ChatOnline() {
+function ChatOnline({ onlineUsers, currentId, setCurrentChat }) {
+    const [friends, setFriends] = useState([]);
+    const [onlineFriends, setOnlineFriends] = useState([]);
+
+    useEffect(() => {
+        const getFriends = async () => {
+            const res = await api.get("/users/friends/" + currentId);
+            setFriends(res.data);
+        }
+        getFriends();
+    }, [currentId]);
+
+    useEffect(() => {
+        setOnlineFriends(friends.filter((f) => onlineUsers.includes(f._id)));
+    }, [friends, onlineUsers]);
+
+    // console.log('onlineFriends:', onlineFriends);
+
     return (
         <>
             <div className="chatOnline">
-                <div className="chatOnlineFriend">
-                    <div className="chatOnlineImgContainer">
-                        <img
-                            className="chatOnlineImg"
-                            src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                            alt=""
-                        />
-                        <div className="chatOnlineBadge"></div>
+                {onlineFriends.map((o) => (
+                    <div
+                        key={o._id}
+                        className="chatOnlineFriend"
+                        onClick={() => setCurrentChat(o)}>
+                        <div className="chatOnlineImgContainer">
+                            <img
+                                className="chatOnlineImg"
+                                src={
+                                    o?.profilePicture
+                                        ? o.profilePicture
+                                        : "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                                }
+                                alt=""
+                            />
+                            <div className="chatOnlineBadge"></div>
+                        </div>
+                        <span className="chatOnlineName">{o?.username}</span>
                     </div>
-                    <span className="chatOnlineName">John Doe</span>
-                </div>
+                ))}
             </div>
         </>
     )
