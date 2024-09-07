47a0c99d0c689b1f7e6d6afaea567513a091cc86
John Sandoval <jcsandoval978@gmail.com>
2024-09-07 15:09:47 -0400

    Add getConversation endpoint for finding conversations between two users

--- ReactPlayground/ChatApp/server/routes/conversation.routes.js
+++ ReactPlayground/ChatApp/server/routes/conversation.routes.js
@@ -5,4 +5,5 @@ const Conversation = require('../controllers/conversation.controller');
 module.exports = function(app) {
     app.post("/api/conversations", Conversation.createConversation);
     app.get("/api/conversations/:userId", Conversation.getConversations);
+    app.get("/api/conversations/find/:firstUserId/:secondUserId", Conversation.getConversation);
 }
