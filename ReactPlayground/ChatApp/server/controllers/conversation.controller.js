d53eb3f478d433bc0a7de86fb0e35850d7806406
John Sandoval <jcsandoval978@gmail.com>
2024-09-07 15:09:35 -0400

    Add getConversation (2 users) endpoint to conversation controller

--- ReactPlayground/ChatApp/server/controllers/conversation.controller.js
+++ ReactPlayground/ChatApp/server/controllers/conversation.controller.js
@@ -25,3 +25,15 @@ module.exports.getConversations = async (req, res) => {
         res.status(500).json(err);
     }
 };
+
+// Get conversation including two userId's
+module.exports.getConversation = async (req, res) => {
+    try {
+        const conversation = await Conversation.findOne({
+            members: { $all: [req.params.firstUserId, req.params.secondUserId] }
+        });
+        res.status(200).json(conversation);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
